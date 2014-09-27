d7472ebeab8d67640d4b20b47da88737fc11ff3e
Ellebkey <ellebkey@gmail.com>
2014-09-27 11:32:11 -0500

    Deteccion de color e imprime cuando lo encuentra

--- RedDetect.py
+++ RedDetect.py
@@ -1,20 +1,47 @@
 import cv2
 import numpy as np
+ 
+cap = cv2.VideoCapture(0)
+ 
+while(1):
+ 
+    # Take each frame
+    _, frame = cap.read()
 
-cam=cv2.VideoCapture(0)
-n=0
-
-while True:
-    returnVal,frame=cam.read()
-
-    img=cv2.GaussianBlur(frame, (5,5), 0)
-    img=cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-    red_lower=np.array([0,150,0],np.uint8)
-    red_upper=np.array([10,255,255],np.uint8)
-    red=cv2.inRange(img,red_lower,red_upper)
-
-    cv2.imshow('img',red)
-
-    key = cv2.waitKey(10) % 0x100
-    if key == 27: break #ESC 
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+ 
+    # define range of blue color in HSV
+    lower_blue = np.array([170,150,60])
+    upper_blue = np.array([179,255,255])
+ 
+    # Threshold the HSV image to get only blue colors
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+ 
+    # Bitwise-AND mask and original image
+    res = cv2.bitwise_and(frame,frame, mask= mask)
+    moments = cv2.moments(mask)
+    area = moments['m00']
+    """if area > 100000:
+        print "Es azul"
+    else:
+        print "No es azul"
+    """
+    '''print " -----------------Frame ---------------"
+    print frame
+    print "------------------Res-----------------"
+    print  res
+    raw_input()
+    '''
+ 
+    """if  res:
+        print "yes"
+        raw_input()"""
+ 
+    cv2.imshow('frame',frame)
+    cv2.imshow('mask',mask)
+    cv2.imshow('res',res)
+    k = cv2.waitKey(5) & 0xFF
+    if k == 27:
+        break
+ 
+cv2.destroyAllWindows()
